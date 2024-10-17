004c0eada8df0446511754c635e353f2aaa1f422
Philip Kotoulas <fkotoulas@thehomli.com>
2024-10-17 11:56:48 +0300

    feat: add front

---
+++ summary_app/frontend/src/components/LoadingItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="loading-card">
+    <div class="loading-line"></div>
+    <div class="loading-line"></div>
+    <div class="loading-line short"></div>
+    <div class="loading-line short"></div>
+  </div>
+</template>
+
+<script setup>
+// No extra logic required for this simple component
+</script>
+
+<style scoped>
+.loading-card {
+  width: 100%;
+  height: 130px;
+  padding: 10px;
+  border: 1px solid #e0e0e0;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: #f9f9f9;
+  margin-bottom: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.loading-line {
+  height: 15px;
+  background-color: #e0e0e0;
+  border-radius: 5px;
+  animation: loading 1.5s infinite;
+}
+
+.loading-line.short {
+  width: 70%;
+}
+
+@keyframes loading {
+  0% {
+    background-color: #e0e0e0;
+  }
+  50% {
+    background-color: #d0d0d0;
+  }
+  100% {
+    background-color: #e0e0e0;
+  }
+}
+</style>
---
+++ summary_app/frontend/src/components/Modal.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="modal-backdrop" v-if="isVisible" @click="closeModal">
+    <div class="modal-content" @click.stop>
+      <h3>Topics List</h3>
+      <ul>
+        <li v-for="(topic, index) in summary?.comments" :key="index">
+          {{ topic }}
+        </li>
+      </ul>
+      <div class="close" @click="closeModal">x</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, ref } from 'vue'
+
+// Define the props
+const props = defineProps({
+  summary: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+  isVisible: {
+    type: Boolean,
+    required: true,
+  },
+})
+
+// Define the emits
+const emit = defineEmits(['close'])
+
+// Function to close the modal
+const closeModal = () => {
+  emit('close')
+}
+</script>
+
+<style scoped>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-content {
+  position: relative;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  width: 800px;
+}
+
+.modal-content ul {
+  margin-top: 20px;
+  min-height: 500px;
+  max-height: 80vh;
+  overflow-y: auto;
+}
+
+.close {
+  margin-top: 0;
+  padding: 10px 15px;
+  background-color: #dddddd;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #969696;
+}
+
+.close:hover {
+  background-color: #3f3f3f;
+}
+</style>
